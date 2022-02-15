08bd5e152822467a9456db2ad7b5fb01195fcf80
Theophilus Tettey <theophilus.tettey@amalitech.org>
2022-02-15 13:42:15 +0000

    project first commit

---
+++ gameboard.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $('.more-btn').on('click', event => {
+        $(event.currentTarget).siblings('.more-menu').toggle();
+    })
+    $('.share').on('click', event => {
+        $(event.currentTarget).next().toggle();
+    })
+    $('.notification').on('click', event => {
+        $(event.currentTarget).toggleClass('active');
+    })
+});
